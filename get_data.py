c5e14cd9432e4dd927aa8be8ccc6bbbfaad2aad1
Naz2204 <ngvoznyuk@gmail.com>
2024-12-19 13:56:14 +0200

    Initial commit

---
+++ get_data.py
@@ -0,0 +1,12 @@
+class GetData:
+    def __init__(self):
+        self.location: tuple[int, int] = None
+
+    def __get_coordinates(self): pass
+
+
+    def get_nearest_locality(self): pass
+
+    def get_speed(self): pass
+
+    def get_speed_limit(self): pass
