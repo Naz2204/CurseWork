a08e5f9c5908925b9b6a40fbea7255cc73ee7b37
Naz2204 <ngvoznyuk@gmail.com>
2024-12-20 19:06:42 +0200

    Added functions to find closes fuel station and coordinates

--- get_data.py
+++ get_data.py
@@ -1,12 +1,125 @@
+from kivy.app import App
+from kivy.utils import platform
+from kivymd.uix.dialog import MDDialog
+
+import requests
+
 class GetData:
     def __init__(self):
-        self.location: tuple[int, int] = None
+        self.location: list = [0, 0]
 
-    def __get_coordinates(self): pass
+    def run(self):
+        #Request permission on Android
+        if platform == "android":
+            from android.permitions import Permission, request_permissions
+            from plyer import gps
+
+            def callback(permission, results):
+                if all([res for res in results]):
+                    print("Permission granted")
+                else:
+                    print("Permission granted")
+            #asks for permittion
+            request_permissions([Permission.ACCESS_COARSE_LICATION, Permission.ACCESS_FINE_LOCATION], callback)
+
+            gps.configure(on_location=self.__get_coordinates,
+                            on_status=self.__on_auth_status)
+            gps.start(minTime=1000)
 
 
-    def get_nearest_locality(self): pass
+    def __get_coordinates(self, *args, **kwargs):
+        self.location[0] = kwargs['lat']
+        self.location[1] = kwargs['lon']
+
+    def __on_auth_status(self, general_status, status_message):
+        if general_status == "provider-enabled":
+            print("GPS found")
+        else:
+            #TODO викликати вікно з попередженням що потрібний GPS
+            pass
+
+    def GPS_turned_off_popup(self):
+        dialog = MDDialog(title = "GPS Error", text = "For app functionality turn GPS on")
+        dialog.size_hint = (0.8, 0.8)
+        dialog.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        dialog.open()
+
+    def get_nearest_locality(self):
+        pass
+
+    def get_closest_fuel_station(self):
+        latitude, longitude = self.location
+        # Define a small bounding box around the point
+        fuel_station_url = f"https://overpass-api.de/api/interpreter"
+        fuel_station_query = f"""
+                [out:json];
+                (
+                    node["amenity"="fuel"](around:1000,{latitude},{longitude}); 
+                    way["amenity"="fuel"](around:1000,{latitude},{longitude});
+                ); 
+                out center;
+                """
+
+        rout_url = "http://router.project-osrm.org/route/v1/driving/"
+
+        response = requests.get(fuel_station_url, params={'data': fuel_station_query})
+
+        if response.status_code == 200:
+            data = response.json()
+            closest_station = None
+            closest_distance = 999999999999999
+            for element in data['elements']:
+                station_coords = None
+                if element['type'] == 'way':
+                    station_coords = [element['center']['lon'], element['center']['lat']]
+                else:
+                    station_coords = [element['lat'], element['lon']]
+
+                distance_result = requests.get(rout_url + longitude + "," + latitude + ";" + station_coords[0]+
+                                        "," + station_coords[1] + "?overview=false")
+
+                if distance_result.status_code == 200:
+                    distance_result = distance_result.json()
+                    if distance_result["code"] == "Ok" and distance_result["routes"]["distance"]:
+                        distance = distance_result["routes"]["distance"]
+                    else:
+                        distance = 999999999999999
+
+                    if element['name'] and distance < closest_distance:
+                        closest_station = element['name']
+
+            if not closest_station:
+                return "No fuel stations found."
+            else:
+                return closest_station
+        else:
+            print("Overpass API error:", response.status_code, response.text)
+            return None
+
 
     def get_speed(self): pass
 
-    def get_speed_limit(self): pass
+
+    def get_speed_limit(self):
+        latitude, longitude = self.location
+        # Define a small bounding box around the point
+        bbox = f"{latitude - 0.0001},{longitude - 0.0001},{latitude + 0.0001},{longitude + 0.0001}"
+
+        url = f"https://overpass-api.de/api/interpreter"
+        query = f"""
+                [out:json];
+                way
+                  [highway]
+                  ( {bbox} );
+                out tags;
+                """
+        response = requests.get(url, params={'data': query})
+        if response.status_code == 200:
+            data = response.json()
+            for element in data['elements']:
+                if 'maxspeed' in element['tags']:
+                    return element['tags']['maxspeed']
+            return "No speed limit found."
+        else:
+            print("Overpass API error:", response.status_code, response.text)
+            return None
--- main.py
+++ main.py
@@ -1,69 +1,8 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-
-
-class MainApp(App):
-
-    def build(self):
-        self.icon = ""
-        self.operators = ["/", "*", "+", "-"]
-        self.last_operator = None
-        self.last_button = None
-
-        main_layout = BoxLayout(orientation="vertical")
-        self.solution = TextInput(background_color= "black", foreground_color= "white",
-                                  multiline=False, halign="right", font_size=55, readonly=True)
-
-        main_layout.add_widget(self.solution)
-        buttons = [["7", "8", "9", "/"],
-                   ["4", "5", "6", "*"],
-                   ["1", "2", "3", "+"],
-                   [".", "0", "C", "-"]]
-
-        for row in buttons:
-            h_layout = BoxLayout(orientation="horizontal")
-            for label in row:
-                button = Button(
-                    text=label, font_size=30, background_color= "gray",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5}
-                )
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
-
-        equal_button = Button(text="=", font_size=30, background_color= "gray",
-                              pos_hint={"center_x": 0.5, "center_y": 0.5})
-        equal_button.bind(on_press=self.on_solution)
-        main_layout.add_widget(equal_button)
-
-        return main_layout
-
-    def on_button_press(self, button):
-        current = self.solution.text
-        button_text = button.text
-
-        if button_text == "C":
-            self.solution.text = ""
-        else:
-            if current and (self.last_operator and button_text in self.operators):
-                return
-            elif current == "" and button_text in self.operators:
-                return
-            else:
-                new_text = current + button_text
-                self.solution.text = new_text
-        self.last_button = button_text
-        self.last_operator = self.last_button in self.operators
-
-    def on_solution(self, button):
-        text = self.solution.text
-        if text:
-            solution = str(eval(self.solution.text))
-            self.solution.text = solution
-
+from kivymd.app import MDApp
+from get_data import GetData
+class MainApp(MDApp):
+    pass
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+    GetData()
+    MainApp().run()
